3a65245e102301c1f8c786d2a374cfa69e653471
bisalva <bisalva99@gmail.com>
2024-11-27 11:19:38 -0300

    Creation DB + Show Tables

--- DataBase/structure.py
+++ DataBase/structure.py
@@ -1,1 +1,39 @@
-print("hola")
+import sqlite3
+
+# Crea la DB
+conexion = sqlite3.connect("students_database.db")
+
+# Crea el cursor para interactuar con la DB
+cursor = conexion.cursor()
+
+# Crea la tabla
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS estudiantes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    correo TEXT NOT NULL
+)
+""")
+
+
+# Ingresa valores a la tabla
+cursor.execute("INSERT INTO estudiantes (nombre, correo) VALUES ('Juan Pérez', 'juanperez@ejemplo.com')")
+cursor.execute("INSERT INTO estudiantes (nombre, correo) VALUES ('Eduardo Peña', 'edu.peña@ejemplo.com')")
+
+# Confirma los cambios a la tabla
+conexion.commit()
+
+# Consulta los registros totales de la tabla
+cursor.execute("SELECT * FROM estudiantes")
+
+# Obtener los resultados de la Consulta
+estudiantes = cursor.fetchall()
+
+# Mostrar los resultados
+print("\n[Estudiantes]\n")
+for estudiante in estudiantes:
+    print(f"ID: {estudiante[0]}, Nombre: {estudiante[1]}, Correo: {estudiante[2]},")
+
+cursor.close()
+conexion.close()
+
